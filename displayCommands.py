179ec242a0115e3f36e0bf6b22535da93beafc29
DavGreenberg <1kindlekid@gmail.com>
2023-06-23 19:03:51 -0400

    More progress on skill system

--- displayCommands.py
+++ displayCommands.py
@@ -1,52 +1,50 @@
 import time
 
-#displays a string one letter at a time
-#can pass a speed and whether or not the string will end with a newline
-def disp(string, speed = 0.05, end=True):
-  for letter in string:
-    print(letter, end="", flush=True)
-    time.sleep(speed)
-  else:
-    if (not end):
-      return
-    print("")
+# displays a string one letter at a time
+# can pass a speed and whether or not the string will end with a newline
 
-#displays the health and name for a character
+
+def disp(string, speed=0.05, end=True):
+    for letter in string:
+        print(letter, end="", flush=True)
+        time.sleep(speed)
+    else:
+        if (not end):
+            return
+        print("")
+
+# displays the health and name for a character
+
+
 def dispFightStats(character):
-  name = character.name
-  hp = character.hp
-  maxHp = character.maxHp
-  numBoxes = 20
+    name = character.name
+    hp = character.hp
+    maxHp = character.maxHp
+    numBoxes = 20
 
-  pctHp = float(hp) / maxHp
-  healthBar = "▮" * round(pctHp * numBoxes)
-  healthBar += "▯" * (numBoxes - len(healthBar))
-  
-  disp(name)
-  disp(f"{healthBar} {round(pctHp * 100)}% ({hp}/{maxHp})")
+    pctHp = float(hp) / maxHp
+    healthBar = "▮" * round(pctHp * numBoxes)
+    healthBar += "▯" * (numBoxes - len(healthBar))
+
+    disp(name)
+    disp(f"{healthBar} {round(pctHp * 100)}% ({hp}/{maxHp})")
+
 
 def dispFightMenu():
-  disp("Choose what to do (attack, item, run):")
-  return input("")
+    disp("Choose what to do (attack, item, run):")
+    return input("")
+
 
 def dispMainMenu():
-  print("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/")
-  disp("Choose what to do (item, resume, exit)")
-  print("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/")
-  
-  
-def getAction(action):
-  match action:
-    case "attack":
-      
-    case "item":
-      
-    case "run":
-      
-    case _:
-      disp("That was not a valid action.")
-      getAction(dispFightMenu().lower())
-      
-def getAttack(skillsAvailable):
-  for skill in skillsAvailable:
-    
+    print("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/")
+    disp("Choose what to do (item, resume, exit)")
+    print("/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/")
+
+
+def dispSkills(currSkills):
+    print("")
+    disp("Skills:")
+    for skill in currSkills:
+        disp(f"{skill}: {currSkills[skill]["stm"]}")
+    disp("Choose a Skill or enter \"Back\" to return: ", end=False)
+    return input()
--- main.py
+++ main.py
@@ -7,42 +7,98 @@ import time
 import json
 
 dc.disp("Welcome to Dungeon Adventure 2")
-dc.disp("What is your name?: ", .025, False)
+dc.disp("What is your name?: ", .04, False)
 pName = input()
 print("")
+dc.disp("World Level -- 1", .1)
 
 p = Player(pName)
 
 enemy = None
 armor = {
-  "Head": ["", None],
-  "Chest": ["", None],
-  "Leg": ["", None],
-  "Boot": ["", None]
+    "Head": ["", None],
+    "Chest": ["", None],
+    "Leg": ["", None],
+    "Boot": ["", None]
 }
 
 i = open("items.json")
 items = json.load(i)
 i.close()
 
+currSkills = {}
+
 worldLevel = 1
+
+# returns a new instance of a monster from the enemies folder
 
 
 def chooseMonster(atk, spd, df, wl):
-  enemies = os.listdir('Enemies')
-  enemyInd = random.randint(0, len(enemies) - 2)
-  if (enemies[enemyInd] == "Orc.py"):
-    return Orc(atk, spd, df, wl)
+    enemies = os.listdir('Enemies')
+    enemyInd = random.randint(0, len(enemies) - 2)
+    if (enemies[enemyInd] == "Orc.py"):
+        return Orc(atk, spd, df, wl)
+
+# updates the player's skills based on skills.json
 
 
-#main game loop
+def updateCurrSkills(player):
+    skillsJSON = open("skills.json")
+    allSkills = json.load(skillsJSON)
+    skillsJSON.close()
+
+    global currSkills
+    newSkills = []
+    for skill in allSkills:
+        if (currSkills.has_key(skill)):
+            continue
+
+        if (allSkills[skill]["lvl"] <= player.lvl):
+            newSkills.append(skill)
+            currSkills[skill] = allSkills[skill]
+
+    return newSkills
+
+
+fightActions = ["attack", "item", "run"]
+# small function to handle actions
+
+
+def checkAction(action):
+    if (action not in fightActions):
+        dc.disp("That was not a valid action.")
+        checkAction(dc.dispFightMenu().lower())
+
+
+def checkSkills(action):
+    if (action not in list(currSkills.keys) or action != "back"):
+        dc.disp("That was not a valid skill.")
+        dc.disp("Choose a Skill or enter \"Back\" to return: ", end=False)
+        checkSkills(input())
+
+
+# main game loop
 while True:
-  #choose enemy type
-  enemyAtk = worldLevel * 2
-  enemySpd = worldLevel
-  enemyDf = (worldLevel+1) * 2
-  enemy = chooseMonster(enemyAtk, enemySpd, enemyDf, worldLevel)
-  dc.dispFightStats(p)
-  dc.dispFightStats(enemy)
-  action = dc.dispFightMenu().lower()
-  dc.getAction(action)
+    # generate enemy stats (this might be better handled within each enemy file for enemy variety)
+    enemyAtk = worldLevel * 2
+    enemySpd = worldLevel
+    enemyDf = (worldLevel+1) * 2
+
+    # choose enemy
+    enemy = chooseMonster(enemyAtk, enemySpd, enemyDf, worldLevel)
+
+    # begin fight -- needs to be within its own game loop
+    # TODO: create better way to handle menu system
+    dc.dispFightStats(p)
+    dc.dispFightStats(enemy)
+    action = dc.dispFightMenu().lower()
+    checkAction(action)
+    match action:
+        case "attack":
+            chosen = dc.dispSkills(currSkills)
+            checkSkills(chosen)
+
+    # this should occur AFTER every fight, thus outside of fight loop
+    newSkills = updateCurrSkills(p)
+    for skill in newSkills:
+        dc.disp(f"You have unlocked a new Skill!: {skill}")
